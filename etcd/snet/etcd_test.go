f2edf0adb5bcbb0624f1c8a8150f3543e87e16c7
Alexander Scherbatiy <stellarspot@gmail.com>
2018-10-01 19:12:19 +0300

    Calculate requests per seconds

--- etcd/snet/etcd_test.go
+++ etcd/snet/etcd_test.go
@@ -3,6 +3,7 @@ package snet
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/DATA-DOG/godog"
@@ -61,6 +62,15 @@ func (client *Client) ToString() string {
 	)
 }
 
+func (client *Client) GetKey() string {
+	return "Key"
+}
+
+func (client *Client) GetValue() string {
+	return "Value"
+}
+
+var debug = false
 var endpoints []string = make([]string, 0)
 var clients []Client = make([]Client, 0)
 var readsNum int
@@ -107,6 +117,11 @@ func eachOfThemWritesTimesAndReadsTimes(writes int, reads int) error {
 
 func allResultsShouldSucceed() error {
 
+	totalRequets := 0
+	readRequests := 0
+	writeRequests := 0
+	start := time.Now()
+
 	fmt.Println("call etcd client")
 
 	cli, err := clientv3.New(clientv3.Config{
@@ -119,25 +134,50 @@ func allResultsShouldSucceed() error {
 	}
 	defer cli.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	putResponse, putErr := cli.Put(ctx, "key3", "value3")
-	cancel()
+	for _, client := range clients {
 
-	if putErr != nil {
-		return putErr
+		key := client.GetKey()
+		value := client.GetValue()
+
+		for i := 0; i < writesNum; i++ {
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			putResponse, err := cli.Put(ctx, key, value)
+			cancel()
+
+			if err != nil {
+				return err
+			}
+
+			if debug {
+				fmt.Println("put response: ", putResponse)
+			}
+
+			writeRequests++
+			totalRequets++
+		}
+
+		for j := 0; j < readsNum; j++ {
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			getResponse, err := cli.Get(ctx, key)
+
+			cancel()
+			if err != nil {
+				return err
+			}
+
+			if debug {
+				fmt.Println("get response: ", getResponse)
+			}
+
+			readRequests++
+			totalRequets++
+		}
 	}
 
-	fmt.Println("put response: ", putResponse)
-
-	ctx, cancel = context.WithTimeout(context.Background(), timeout)
-	getResponse, getErr := cli.Get(ctx, "key3")
-
-	cancel()
-	if getErr != nil {
-		return getErr
-	}
-
-	fmt.Println("get response: ", getResponse)
+	elapsed := time.Now().Sub(start).Seconds()
+	requestsPerTime := float64(totalRequets) / float64(elapsed)
+	fmt.Println("total requests: ", totalRequets, "write requests: ", writeRequests, "read requests: ", readRequests)
+	fmt.Println("elapsed time in seconds: ", elapsed, "requests per seconds: ", strconv.FormatFloat(requestsPerTime, 'f', 2, 64))
 
 	return nil
 }
