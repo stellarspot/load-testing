6cb9b1a5f01bafaeb72f3316fc5317a50aad754b
Alexander Scherbatiy <stellarspot@gmail.com>
2018-10-09 17:53:09 +0300

    Move etcd server start logic to etcd_storage_server.go file

--- etcd/snet/etcd_server_test.go
+++ etcd/snet/etcd_server_test.go
@@ -3,22 +3,21 @@ package snet
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"strconv"
 	"sync"
-	"time"
 
 	"go.etcd.io/etcd/embed"
 )
 
-var serverMutex sync.Mutex
+const (
+	token = "cluster-token"
+)
 
 var names []string
 var clientEndpoints []string
 var peerEndpoints []string
 
 var etcdServers []*embed.Etcd
-var serversWaitGroup sync.WaitGroup
 
 func etcdInstancesNamesAre(values string) error {
 	names = split(values)
@@ -59,22 +58,41 @@ func etcdClusterConsistsOfNodes(nodesNum int) error {
 
 func ectdClusterIsRun() error {
 
-	serversWaitGroup.Add(len(names))
-	var results sync.Map
+	var lock sync.Mutex
+	var wg sync.WaitGroup
+	var results map[string]error
+
+	wg.Add(len(names))
 
 	for index, name := range names {
-		go runServer(name, clientEndpoints[index], peerEndpoints[index], results)
+
+		go func(name string, clientEndpoint string, peerEndPoint string) {
+			defer wg.Done()
+
+			initialCluster := getInitialCluster()
+			etcd, err := StartEtcdServer(name, clientEndpoint, peerEndPoint, initialCluster, token)
+
+			lock.Lock()
+			defer lock.Unlock()
+
+			if err != nil {
+				results[name] = err
+				return
+			}
+
+			etcdServers = append(etcdServers, etcd)
+
+		}(name, clientEndpoints[index], peerEndpoints[index])
 	}
 
-	serversWaitGroup.Wait()
+	wg.Wait()
 
 	foundError := false
 
-	results.Range(func(k, v interface{}) bool {
+	for name, err := range results {
 		foundError = true
-		fmt.Printf("server: %v, error: %v\n", k, v)
-		return true
-	})
+		fmt.Printf("server: %v, error: %v\n", name, err)
+	}
 
 	if foundError {
 		return errors.New("Some servers fail to start. See output for more details")
@@ -102,66 +120,4 @@ func getInitialCluster() string {
 		initialCluster += name + "=" + peerEndpoints[index]
 	}
 	return initialCluster
-}
-
-func runServer(name string, clientEndpoint string, peerEndPoint string, results sync.Map) {
-
-	defer serversWaitGroup.Done()
-
-	clientURL, err := url.Parse(clientEndpoint)
-
-	if err != nil {
-		results.Store(name, err)
-		return
-	}
-
-	peerURL, err := clientURL.Parse(peerEndPoint)
-
-	if err != nil {
-		results.Store(name, err)
-		return
-	}
-
-	cfg := embed.NewConfig()
-	cfg.Name = name
-	cfg.Dir = name + ".etcd"
-
-	// --listen-client-urls
-	cfg.LCUrls = []url.URL{*clientURL}
-
-	// --advertise-client-urls
-	cfg.ACUrls = []url.URL{*clientURL}
-
-	// --listen-peer-urls
-
-	cfg.LPUrls = []url.URL{*peerURL}
-
-	// --initial-advertise-peer-urls
-	cfg.APUrls = []url.URL{*peerURL}
-
-	// --initial-cluster
-	initialCluster := getInitialCluster()
-	cfg.InitialCluster = initialCluster
-
-	//  --initial-cluster-state
-	cfg.ClusterState = embed.ClusterStateFlagNew
-
-	etcdServer, err := embed.StartEtcd(cfg)
-
-	if err != nil {
-		results.Store(name, err)
-		return
-	}
-
-	serverMutex.Lock()
-	etcdServers = append(etcdServers, etcdServer)
-	serverMutex.Unlock()
-
-	select {
-	case <-etcdServer.Server.ReadyNotify():
-	case <-time.After(10 * time.Second):
-		etcdServer.Server.Stop()
-		results.Store(name, errors.New("etcd server took too long to start"))
-		return
-	}
 }
